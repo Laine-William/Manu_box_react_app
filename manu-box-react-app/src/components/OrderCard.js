52cd9ea6e8582e4a9ed26727c29d7743f44691db
lwdu76 <laine.william01@gmail.com>
2022-11-18 12:45:06 +0100

    update

--- manu-box-react-app/src/components/OrderCard.js
+++ manu-box-react-app/src/components/OrderCard.js
@@ -7,17 +7,22 @@ import { Card,
 import { useThemeHook } from '../GlobalComponents/ThemeProvider';
 
 const OrderCard = (props) => {
+
     const [theme] = useThemeHook();
+    
     return (
-       <Card className={`mb-3`} border={theme? 'warning' : 'primary'}>
+    
+        <Card className={`mb-3`} border={theme? 'warning' : 'primary'}>
             <Card.Header>
                 <b>{props.box}</b>
                 <small className="float-end">{props.boxId}</small>
             </Card.Header>
+        
             <Row className="p-2">
                 <Col xs={3} sm={2}>
                     <Card.Img variant="top" src={props.image} />
                 </Col>
+        
                 <Col>
                     <Card.Body>
                         <Card.Title>{props.title}</Card.Title>
