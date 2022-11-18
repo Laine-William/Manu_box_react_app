e906e89d6566e7425eb9cc826f55dd1e5b428943
lwdu76 <laine.william01@gmail.com>
2022-11-18 12:42:10 +0100

    update

--- manu-box-react-app/src/App.js
+++ manu-box-react-app/src/App.js
@@ -13,10 +13,8 @@ import MyAccount from "./Pages/MyAccount";
 
 function App() {
 
-  const [theme] = useThemeHook();
-
   return (
-    
+
     <main className={'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
 
       <Header/>
