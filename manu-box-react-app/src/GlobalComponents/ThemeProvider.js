d084b7dd667ecb679ee2505faf669ffa481493f9
lwdu76 <laine.william01@gmail.com>
2022-11-18 12:01:51 +0100

    update

--- manu-box-react-app/src/GlobalComponents/ThemeProvider.js
+++ manu-box-react-app/src/GlobalComponents/ThemeProvider.js
@@ -1,16 +1,24 @@
-import React, { createContext, useState, useEffect, useContext} from 'react';
+import React, { createContext, 
+                useState, 
+                useEffect, 
+                useContext} from 'react';
 
 const ThemeContext = createContext();
 
 const ThemeProvider = (props) => {
+
     const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || false );
     
-    useEffect(()=>{
-        localStorage.setItem('theme', JSON.stringify(theme));
-    },[theme]);
+    useEffect (() => {
 
-    const setThemeMode = mode => setTheme(mode);
+        localStorage.setItem ('theme', JSON.stringify (theme));
+
+    }, [theme]);
+
+    const setThemeMode = mode => setTheme (mode);
+
     return (
+
         <ThemeContext.Provider value={{ theme, setThemeMode}}>
             {props.children}
         </ThemeContext.Provider>
@@ -18,7 +26,9 @@ const ThemeProvider = (props) => {
 };
 
 const useThemeHook = () =>{
+
     const {theme} = useContext(ThemeContext);
+    
     return [theme];
 }
 
