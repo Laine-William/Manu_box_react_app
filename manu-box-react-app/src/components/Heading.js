e69ceaa6330f0b476657878c32cb094c6389e8a4
lwdu76 <laine.william01@gmail.com>
2022-11-18 12:07:13 +0100

    update

--- manu-box-react-app/src/components/Heading.js
+++ manu-box-react-app/src/components/Heading.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Heading = (props) => {
 
     return (
+
         <h1 className={`text-center border-bottom py-2 ${props?.size} ${'text-light-primary'}`}>
+            
             {props.heading}
         </h1>
     );
