1d7e22441095f34cc90f7a976a108582ffe5e786
lwdu76 <laine.william01@gmail.com>
2022-11-18 14:21:46 +0100

    update

--- manu-box-react-app/src/App.js
+++ manu-box-react-app/src/App.js
@@ -1,31 +1,36 @@
-
+// Importe les npm install
 import React from "react";
 
-
+// Importe 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+// Importe l'en tête
 import Header from './components/Header';
 
-
+// Importe la route
 import { Router } from "@reach/router";
 
-
+// Importe les pages
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import MyAccount from "./Pages/MyAccount";
 
-
+// Fonction App qui retourne la page principale
 function App() {
 
+  // Retourne le contenu
   return (
 
+    // Style principal
     <main className={'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
 
       <Header/>
 
+      {/* Route */}
       <Router>
+
+        {/* Chemin des fichiers */}
         <Home path="/" />
         <MyAccount path="my-account" />
         <Cart path="/cart" />
