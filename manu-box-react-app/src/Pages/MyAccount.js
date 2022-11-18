74ad090a00048e438d8ba27e4261ab38cb9c034f
lwdu76 <laine.william01@gmail.com>
2022-11-18 14:18:02 +0100

    update

--- manu-box-react-app/src/Pages/MyAccount.js
+++ manu-box-react-app/src/Pages/MyAccount.js
@@ -1,39 +1,52 @@
-
+// Importe les npm install
 import React from 'react';
 
-
+// Importe les composants de bootstrap
 import { Container,
          Row, 
          Col, 
          Tab, 
          Nav} from 'react-bootstrap';
 
-
+// Importe la rubrique
 import Heading from  '../components/Heading';
 
-
+// Importe les icones
 import { FaClipboardList, 
          FaUser } from 'react-icons/fa';
 import { IoLocationSharp } from 'react-icons/io5';
 
-
+// Importe la feuille de style my-account
 import './my-account.css';
 
-
+// Importe la commande
 import OrderCard from '../components/OrderCard';
 
-
+// Fonction fleche MyAccount qui retoune le contenu du panier
 const MyAccount = () => {
 
+    // Retourne le contenu
     return (
-        
+
+        // Conteneur avec un marge interne et externe
         <Container className="py-5 mt-5">
+
+            {/* Rubrique nos services */}
             <Heading heading="Nos services" />
+
+            {/* Tableau du conteneur des offres */}
             <Tab.Container defaultActiveKey="mon-offre">
+
+                {/* Ligne pour ajuster le contenu */}
                 <Row className="justify-content-evenly mt-4 p-1">
+
+                    {/* Colonne */}
                      <Col sm={3} className={`text-black bg-light p-2 rounded h-100 mb-3 user-menu`}>
+                        
+                        {/* Ligne */}
                         <Row className="mb-3 py-2"></Row>
 
+                        {/* Liens avec un items nos offres et un icone */}
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item className="mb-3">
                                 <Nav.Link eventKey="mon-offre">
@@ -41,11 +54,13 @@ const MyAccount = () => {
                                     <FaClipboardList size="1.4rem" />
                                 </Nav.Link>
 
+                                {/* Liens avec un items details et un icone */}
                                 <Nav.Link eventKey="offre-detail">
                                     La Manu box en details
                                     <FaUser size="1.4rem" />
                                 </Nav.Link>
 
+                                {/* Liens avec un items contact et un icone */}
                                 <Nav.Link eventKey="contact">
                                     La Manu box contact
                                     <IoLocationSharp size="1.4rem" />
@@ -54,10 +69,19 @@ const MyAccount = () => {
                         </Nav>
                      </Col>
 
-                     <Col sm={8} className={`text-black bg-light p-2 rounded`}>
+                     {/* Colonne de nos offres */}
+                     <Col sm = {8} className={`text-black bg-light p-2 rounded`}>
+
+                        {/* Contenu du tableau */}
                         <Tab.Content>
+
+                            {/* Onglet mon offre */}
                             <Tab.Pane eventKey="mon-offre">
+
+                                {/* Appel la rubrique nos offres */}
                                 <Heading heading="Nos offres de box sans engagement" size="h3" />
+
+                                {/* Appel la box S */}
                                 <OrderCard 
                                     boxId="Taille de la box"
                                     title="S"
@@ -65,13 +89,15 @@ const MyAccount = () => {
                                     description="Boxes S de 2 à 3 m² : 50€ HT / mois"
                                 />
 
+                                {/* Appel la box M */}
                                 <OrderCard 
                                     boxId="Taille de la box"
                                     title="M"
                                     image="images/box_size.jpg"
                                     description="Boxes M de 3 à 6 m² : 80€ HT / mois"
                                 />
-
+                                
+                                {/* Appel la box L */}
                                 <OrderCard 
                                     boxId="Taille de la box"
                                     title="L"
@@ -79,6 +105,7 @@ const MyAccount = () => {
                                     description="Boxes L de 6 à 10 m² : 140€ HT / mois"
                                 />
 
+                                {/* Appel la box XL */}
                                 <OrderCard 
                                     boxId="Taille de la box"
                                     title="XL"
@@ -87,8 +114,13 @@ const MyAccount = () => {
                                 />
                             </Tab.Pane>
 
+                            {/* Onglet offre-details */}
                             <Tab.Pane eventKey="offre-detail">
+                                
+                                {/* Rubrique */}
                                 <Heading heading="Detail de l'offre" size="h3" />
+                                
+                                {/* Appel l'offre detaille */}
                                 <OrderCard 
                                     boxId="Details"
                                     title="Les emplacements de stockage sont sécurisés par vidéosurveillance, avec alarme vol et incendie, et sont accessibles 7j/7 par badge personnalisé."
@@ -96,8 +128,13 @@ const MyAccount = () => {
                                 />
                             </Tab.Pane>
 
-                            <Tab.Pane eventKey="contact">
-                                <Heading heading="Contact" size="h3" />
+                            {/* Onglet contact */}
+                            <Tab.Pane eventKey = "contact">
+                                
+                                {/* Rubrique contact */}
+                                <Heading heading = "Contact" size = "h3" />
+                                
+                                {/* Appel l'adresse */}
                                 <OrderCard 
                                     boxId="Adresse"
                                     image="images/manu.png"
