1cc6cc8be27fb5da551ff8b896cbcd18464233bf
lwdu76 <laine.william01@gmail.com>
2022-11-19 07:33:27 +0100

    update

--- manu-box-react-app/src/components/OrderCard.js
+++ manu-box-react-app/src/components/OrderCard.js
@@ -20,30 +20,39 @@ const OrderCard = (props) => {
     return (
     
         // 
-        <Card className={`mb-3`} border={theme? 'warning' : 'primary'}>
+        <Card className = {`mb-3`} border = {theme ? 'warning' : 'primary'}>
 
-            {/*  */}
+            {/* En tete de la carte */}
             <Card.Header>
-                <b>{props.box}</b>
-                <small className="float-end">{props.boxId}</small>
+                <b>{ props.box }</b>
+
+                <small className = "float-end">{ props.boxId }</small>
             </Card.Header>
         
-            {/*  */}
-            <Row className="p-2">
-                <Col xs={3} sm={2}>
-                    <Card.Img variant="top" src={props.image} />
+            {/* Marge de la ligne */}
+            <Row className = "p-2">
+
+                {/* Taille de la colonne */}
+                <Col xs = {3} sm = {2}>
+
+                    {/* Image de la carte */}
+                    <Card.Img variant = "top" src = { props.image } />
                 </Col>
 
-                {/*  */}
+                {/* Colonne */}
                 <Col>
 
-                    {/*  */}
+                    {/* Corps de la carte */}
                     <Card.Body>
 
-                        {/*  */}
-                        <Card.Title>{props.title}</Card.Title>
+                        {/* Titre de la carte */}
+                        <Card.Title>{ props.title }</Card.Title>
+
+                        {/* Texte dans la carte */}
                         <Card.Text>
-                            <Badge pill bg="success">{props.description}</Badge>
+
+                            {/* Description en vert */}
+                            <Badge pill bg = "success">{ props.description }</Badge>
                         </Card.Text>
                     </Card.Body>
                 </Col>
