5ccd68ff24cf7b6f8a348d832b0b54041dbbfef2
lwdu76 <laine.william01@gmail.com>
2022-11-19 08:01:44 +0100

    update

--- manu-box-react-app/src/components/ProductCard.js
+++ manu-box-react-app/src/components/ProductCard.js
@@ -25,42 +25,48 @@ const ProductCard = (props) => {
     //console.log(props.data);
     }
 
-    // 
+    // Retourne le contenu
     return (
 
         // 
         <Card 
-            style={{ width: '18rem', height: 'auto' }}
-                     className={`text-center p-0 overflow-hidden shadow mx-auto mb-4`}>
+            style = {{ width: '18rem', height: 'auto' }}
+                className = {`text-center p-0 overflow-hidden shadow mx-auto mb-4` }>
 
                 {/*  */}
-                <div style={{ background: 'white', height: '15rem', overflow: 'hidden', display: 'flex',
-                justifyContent: 'center', alignItems: 'center', marginBottom: 'inherit' }}>
+                <div style = {{ background : 'white', 
+                                height : '15rem', 
+                                overflow : 'hidden', 
+                                display: 'flex',
+                                justifyContent : 'center', 
+                                alignItems : 'center', 
+                                marginBottom: 'inherit' }}>
 
                     {/*  */}
-                    <div style={{ width: '9rem'}}>
-                        <Card.Img variant="top" src={image} className="img-fluid" />
+                    <div style = {{ width : '9rem' }}>
+
+                        {/* Image de la carte */}
+                        <Card.Img variant = "top" src = {image} className = "img-fluid" />
                     </div>
                 </div>
             
-            {/*  */}
+            {/* Corps de la carte */}
             <Card.Body>
 
-                {/*  */}
-                <Card.Title style={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'}}>
+                {/* Titre de la carte */}
+                <Card.Title style = {{ textOverflow : 'ellipsis',  overflow : 'hidden', whiteSpace : 'nowrap' }}>
                     {title}
                 </Card.Title>
 
-                {/*  */}
+                {/* Taille de la carte en m² */}
                 <Card.Title>
-                    <span className="h3">{price}</span> m²
+                    <span className = "h3">{ price }</span> m²
                 </Card.Title>
 
-                {/*  */}
+                {/* Bouton ajouter */}
                 <Button
-                    onClick={()=> addToCart()}
-                    
-                    className={`d-flex align-item-center m-auto border-0`}>
+                    onClick = { () => addToCart () }                    
+                    className = { `d-flex align-item-center m-auto border-0` }>
                     Ajouter
                 </Button>
             </Card.Body>
